4c6d1314117c36f1af68e9390a40366ebd0ed33e
Kyan Jeuring <63996544+KyanJeuring@users.noreply.github.com>
2024-09-15 16:14:40 +0200

    2024W37D15a

---
+++ WebDev/Week2/DateCalculation/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Date Calculation</title>
+    <link rel="icon" type="image/x-icon" href="../../../Assets/favicon.ico">
+</head>
+<body>
+    <?php
+        $date = 14062016;
+        $day = ($date / 1000000) % 31;
+        $month = ($date / 10000) % 100;
+        $year = $date % 10000;
+        echo "Day:", $day, "\n";
+        echo "Month:", $month, "\n";
+        echo "Year:", $year, "\n";
+    ?>
+</body>
+</html>
