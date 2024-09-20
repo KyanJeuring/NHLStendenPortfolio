c8a954820271fa5790affbbb92a7e327ae87bdde
Kyan Jeuring <63996544+KyanJeuring@users.noreply.github.com>
2024-09-20 11:20:26 +0200

    2024W38D20a

--- WebDev/Week3/Video Store/index.php
+++ WebDev/Week3/Video Store/index.php
@@ -9,8 +9,29 @@
 <body>
     <a href="../../../WebDev.html">&#60;&#60; Back to WebDev Portfolio</a>
     <hr>
+    <form action="./index.php" METHOD="POST">
+        <label for="age">Enter your age: </label>
+        <input type="number" name="age" id="age">
+        <label for="gender">Select your gender:</label>
+        <input type="radio" name="gender" id="gender" value="M">
+        <input type="radio" name="gender" id="gender" value="F">
+    </form>
     <?php
-        
+        function ageChecker()
+        {
+            if ($_POST['age'] < 18)
+            {
+                echo "It looks like your to young to register";
+            }
+        }
+
+        function genderChecker()
+        {
+            if ($_POST['gender' == "F"])
+            {
+                echo "There will be an upcoming ladies night event";
+            }
+        }
     ?>
 </body>
 </html>
