c48d1c130dc571d155785446d8e409b7e75545d7
Kyan Jeuring <63996544+KyanJeuring@users.noreply.github.com>
2024-09-28 12:25:35 +0200

    2024W39D28a

---
+++ WebDev/Week4/Task3b/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Task 3a</title>
+        <link rel="icon" type="image/x-icon" href="../../../Assets/favicon.ico">
+    </head>
+    <body>
+        <a href="../../../WebDev.html">&#60;&#60; Back to WebDev Portfolio</a>
+        <hr>
+        <?php
+        error_reporting(0);
+        echo "<p>Assignment 1</p>";
+        for ($i = 0; $i <= 7; $i++)
+        {
+            for ($j = 0; $j <= 7 - $i; $j++)
+            {
+            echo "&nbsp;&nbsp;&nbsp;";
+            }
+            for ($k = 0; $k <= $i; $k++)
+            {
+            echo " *";
+            }
+            echo "<br>";
+        }
+        echo "<p>Assignment 2</p>";
+        echo "<p>Assignment 3</p>";
+        echo "<p>Assignment 4</p>";
+        echo "<p>Assignment 5</p>";
+        echo "<p>Assignment 6</p>";
+        echo "<p>Assignment 7</p>";
+        echo "<p>Assignment 8</p>";
+        echo "<p>Assignment 9</p>";
+        echo "<p>Assignment 10</p>";
+        echo "<p>Assignment 11</p>";
+        echo "<p>Assignment 12</p>";
+        echo "<p>Assignment 13</p>";
+        echo "<p>Assignment 14</p>";
+        echo "<p>Assignment 15</p>";
+        echo "<p>Assignment 16</p>";
+        echo "<p>Assignment 17</p>";
+        echo "<p>Assignment 18</p>";
+        echo "<p>Assignment 19</p>";
+        echo "<p>Assignment 20</p>";
+        echo "<p>Assignment 21</p>";
+        echo "<p>Assignment 22</p>";
+        echo "<p>Assignment 23</p>";
+        echo "<p>Assignment 24</p>";
+        ?>
+    </body>
+</html>
